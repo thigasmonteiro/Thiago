4698b3706163b2635934247bd82740f5491d79cd
thigasmonteiro <thiagodepaulamonteiro@gmail.com>
2023-05-16 16:01:56 -0300

    criacao de views

--- garagem/views.py
+++ garagem/views.py
@@ -1,3 +1,44 @@
 from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
 
-# Create your views here.
+from garagem.models import Acessorio, Categoria, Cor, Marca, Veiculo
+from garagem.serializers import (AcessorioSerializer, CategoriaSerializer,
+                                 CorSerializer, MarcaSerializer,
+                                 VeiculoDetailSerializer,
+                                 VeiculoListSerializer, VeiculoSerializer)
+
+
+class CategoriaViewSet(ModelViewSet):
+    queryset = Categoria.objects.all()
+    serializer_class = CategoriaSerializer
+
+
+class CorViewSet(ModelViewSet):
+    queryset = Cor.objects.all()
+    serializer_class = CorSerializer
+
+
+class VeiculoViewSet(ModelViewSet):
+    queryset = Veiculo.objects.all()
+    serializer_class = {
+        "list": VeiculoListSerializer,
+        "retrieve": VeiculoDetailSerializer,
+    }
+
+    def get_serializer_class(self):
+        return self.serializer_class.get(self.action, VeiculoSerializer)
+
+
+class AcessorioViewSet(ModelViewSet):
+    queryset = Acessorio.objects.all()
+    serializer_class = AcessorioSerializer
+
+
+class MarcaViewSet(ModelViewSet):
+    queryset = Marca.objects.all()
+    serializer_class = MarcaSerializer
+
+
+class MarcaViewSet(ModelViewSet):
+    queryset = Marca.objects.all()
+    serializer_class = MarcaSerializer
